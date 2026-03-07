204b44a946889554461d72c8bd3f9edca31982df
Sayanel <aymeric.matte.pro@gmail.com>
2026-03-07 16:45:18 +0100

    [common] Clean transform

--- src/common/maths/transform.h
+++ src/common/maths/transform.h
@@ -6,43 +6,43 @@ struct Transform {
 public:
     Transform();
 
-    void setParent(Transform* newParent);
+    void setParent(Transform* _parent);
 
-    void translate(const DirectX::XMFLOAT3& delta);
-    void rotate(const DirectX::XMFLOAT4& deltaQuat);
-    void rotate(const DirectX::XMFLOAT3& deltaEuler);
-    void scale(const DirectX::XMFLOAT3& factor);
+    void translate(const DirectX::XMFLOAT3& _delta);
+    void rotate(const DirectX::XMFLOAT4& _deltaQuat);
+    void rotate(const DirectX::XMFLOAT3& _deltaEuler);
+    void scale(const DirectX::XMFLOAT3& _factor);
 
-    void setPosition(const DirectX::XMFLOAT3& pos);
-    void setRotation(const DirectX::XMFLOAT4& quat);
-    void setRotation(const DirectX::XMFLOAT3& euler);
-    void setScale(const DirectX::XMFLOAT3& scale);
+    void setPosition(const DirectX::XMFLOAT3& _pos);
+    void setRotation(const DirectX::XMFLOAT4& _quat);
+    void setRotation(const DirectX::XMFLOAT3& _euler);
+    void setScale(const DirectX::XMFLOAT3& _scale);
 
-    void localTranslate(const DirectX::XMFLOAT3& delta);
-    void localRotate(const DirectX::XMFLOAT4& deltaQuat);
-    void localRotate(const DirectX::XMFLOAT3& deltaEuler);
-    void localScale(const DirectX::XMFLOAT3& factor);
+    void localTranslate(const DirectX::XMFLOAT3& _delta);
+    void localRotate(const DirectX::XMFLOAT4& _deltaQuat);
+    void localRotate(const DirectX::XMFLOAT3& _deltaEuler);
+    void localScale(const DirectX::XMFLOAT3& _factor);
 
-    void setLocalPosition(const DirectX::XMFLOAT3& pos);
-    void setLocalRotation(const DirectX::XMFLOAT4& quat);
-    void setLocalRotation(const DirectX::XMFLOAT3& euler);
-    void setLocalScale(const DirectX::XMFLOAT3& scale);
+    void setLocalPosition(const DirectX::XMFLOAT3& _pos);
+    void setLocalRotation(const DirectX::XMFLOAT4& _quat);
+    void setLocalRotation(const DirectX::XMFLOAT3& _euler);
+    void setLocalScale(const DirectX::XMFLOAT3& _scale);
 
-    void setWorldTransform(const DirectX::XMFLOAT3& pos, const DirectX::XMFLOAT4& quat, const DirectX::XMFLOAT3& scale);
+    void setWorldTransform(const DirectX::XMFLOAT3& _pos, const DirectX::XMFLOAT4& _quat, const DirectX::XMFLOAT3& _scale);
 
-    DirectX::XMFLOAT3 getLocalPosition() const { return m_localPosition; }
-    DirectX::XMFLOAT4 getLocalRotation() const { return m_localRotation; }
-    DirectX::XMFLOAT3 getLocalScale() const { return m_localScale; }
+    [[nodiscard]] DirectX::XMFLOAT3 getLocalPosition() const { return m_localPosition; }
+    [[nodiscard]] DirectX::XMFLOAT4 getLocalRotation() const { return m_localRotation; }
+    [[nodiscard]] DirectX::XMFLOAT3 getLocalScale() const { return m_localScale; }
 
-    DirectX::XMFLOAT4X4 getWorldMatrix();
-    DirectX::XMFLOAT4X4 getTransposedWorldMatrix();
-    DirectX::XMFLOAT3 getPosition();
-    DirectX::XMFLOAT4 getRotation();
-    DirectX::XMFLOAT3 getScale();
+    [[nodiscard]] DirectX::XMFLOAT4X4 getWorldMatrix();
+    [[nodiscard]] DirectX::XMFLOAT4X4 getTransposedWorldMatrix();
+    [[nodiscard]] DirectX::XMFLOAT3 getPosition();
+    [[nodiscard]] DirectX::XMFLOAT4 getRotation();
+    [[nodiscard]] DirectX::XMFLOAT3 getScale();
 
-    DirectX::XMFLOAT3 getForward();
-    DirectX::XMFLOAT3 getRight();
-    DirectX::XMFLOAT3 getUp();
+    [[nodiscard]] DirectX::XMFLOAT3 getForward();
+    [[nodiscard]] DirectX::XMFLOAT3 getRight();
+    [[nodiscard]] DirectX::XMFLOAT3 getUp();
 
     void lookAt(const DirectX::XMFLOAT3& _target, const DirectX::XMFLOAT3& _up = { 0.f, 1.f, 0.f });
     void lookTo(const DirectX::XMFLOAT3& _direction, const DirectX::XMFLOAT3& _up = { 0.f, 1.f, 0.f });
