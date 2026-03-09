9e1464344421893f9942811ee79aefa70f30b70b
Sayanel <aymeric.matte.pro@gmail.com>
2026-03-09 14:44:25 +0100

    [render] Add driver priority

---
+++ src/render/driverPriority.h
@@ -0,0 +1,13 @@
+#pragma once
+
+extern "C" {
+    typedef unsigned long DWORD;
+
+	// NVIDIA: Choose high-performance GPU
+    __declspec(selectany) __declspec(dllexport)
+    DWORD NvOptimusEnablement = 0x00000001;
+    
+	// AMD: Choose high-performance GPU
+    __declspec(selectany) __declspec(dllexport)
+    int AmdPowerXpressRequestHighPerformance = 1;
+}
